d6ed77ec9440d76a54a6929e494675c6dd7605a4
Louis <louis20019028@gmail.com>
2024-12-11 10:58:24 +0800

    Initial Commit

---
+++ Phone/PhoneApp.swift
@@ -0,0 +1,32 @@
+//
+//  PhoneApp.swift
+//  Phone
+//
+//  Created by Louis Chang on 2024/12/11.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PhoneApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ PhoneTests/PhoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneTests.swift
+//  PhoneTests
+//
+//  Created by Louis Chang on 2024/12/11.
+//
+
+import Testing
+@testable import Phone
+
+struct PhoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
