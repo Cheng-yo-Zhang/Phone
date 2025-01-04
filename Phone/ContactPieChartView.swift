bc14991493e27863c8fcf9bc7e5aa81366928eed
Louis <louis20019028@gmail.com>
2025-01-04 20:34:04 +0800

    update

---
+++ Phone/ContactPieChartView.swift
@@ -0,0 +1,18 @@
+//
+//  ContactPieChartView.swift
+//  Phone
+//
+//  Created by Louis Chang on 2024/12/13.
+//
+
+import SwiftUI
+
+struct ContactPieChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContactPieChartView()
+}
